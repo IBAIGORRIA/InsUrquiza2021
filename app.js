2d3d27ae9bb47b11832fbe971ed682c4469c3a04
Iñaki Baigorria <ixbaigorriaz@gmail.com>
2021-09-11 21:21:14 -0300

    porlas

---
+++ app.js
@@ -0,0 +1,14 @@
+<script>
+var listas=new Array();
+var dniPost=0;
+var codLista=0;
+var lemaLista="";
+var nombreLista="";
+
+function cargaLista() {
+			dniPost=prompt("Ingrese DNI Candidato:"," ", ""),
+            codLista=prompt("ingrese código lista:"," ",""),
+            lemaLista=prompt("ingrese lema lista:"," ",""),
+            nombreLista=prompt("ingrese nombre lista:"," ","")
+}
+</script>
